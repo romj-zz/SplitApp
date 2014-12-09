0bcc1732c48c6fe533c566a6b6d3677968bd3db7
Jean Roman <jroman@moviechainer.com>
2014-12-08 19:55:30 -0500

    connect to web-app

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+function closeWebApp() {
+	document.getElementById('web-app').style.display = 'none';
+	document.getElementById('close').style.display = 'none';
+}
+function openWebApp() {
+	document.getElementById('web-app').style.display = 'block';
+	document.getElementById('close').style.display = 'block';
+}
